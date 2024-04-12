004ce8e2df884fd14d536d038531c5d6ee210a38
Majharul Islam <web.dev.majharul@gmail.com>
2024-04-12 14:47:57 +0600

    add global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,18 @@
-import express from "express";
-
+import express, { Request, Response, NextFunction } from "express";
+import createHttpError, { HttpError } from "http-errors";
+import { config } from "./config/config";
+import globalErrorhandler from "./middlewares/globalErrorhandler";
 const app = express();
 
 //Routes
 app.get("/", (req, res, next) => {
+  const error = createHttpError(400, "Something Went Wrong!");
+  throw error;
   res.json({
     text: "sometext for now",
   });
 });
 
-// app.use();
+app.use(globalErrorhandler);
 
 export default app;
